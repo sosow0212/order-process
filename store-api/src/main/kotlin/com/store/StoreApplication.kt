af0242d0756e4d54e8f5f617b1a31167911576b4
sosow0212 <sosow0212@naver.com>
2024-12-26 22:47:04 +0900

    feat: 모듈 및 프로젝트 생성

---
+++ store-api/src/main/kotlin/com/store/StoreApplication.kt
@@ -0,0 +1,11 @@
+//package com.com.store
+//
+//import org.springframework.boot.autoconfigure.SpringBootApplication
+//import org.springframework.boot.runApplication
+//
+//@SpringBootApplication
+//class StorePaymentApplication
+//
+//fun main(args: Array<String>) {
+//    runApplication<StorePaymentApplication>(*args)
+//}
