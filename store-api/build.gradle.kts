298924aeab92da752059c4bcfb534d872a7f0cd1
sosow0212 <sosow0212@naver.com>
2024-12-30 22:08:04 +0900

    chore: querydsl 추가

--- store-api/build.gradle.kts
+++ store-api/build.gradle.kts
@@ -1,3 +1,9 @@
+plugins {
+    kotlin("kapt")
+}
+
+val queryDslVersion: String by extra
+
 dependencies {
     // security-crypto
     implementation("org.springframework.security:spring-security-crypto")
@@ -6,4 +12,34 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
     implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
+    // QueryDSL 의존성 추가
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    kapt("jakarta.annotation:jakarta.annotation-api")
+    kapt("jakarta.persistence:jakarta.persistence-api")
 }
+
+// Querydsl 설정 시작
+val generated = file("src/main/generated")
+
+tasks.withType<JavaCompile> {
+    options.generatedSourceOutputDirectory.set(generated)
+}
+
+sourceSets {
+    main {
+        kotlin.srcDirs += generated
+    }
+}
+
+tasks.named("clean") {
+    doLast {
+        generated.deleteRecursively()
+    }
+}
+
+kapt {
+    generateStubs = true
+}
+// Querydsl 설정 종료
